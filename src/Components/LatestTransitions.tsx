188b15e5f646269dc2f3215fe27c87b2b40b15cb
Zakria-04 <zakriaabdelgny04@gmail.com>
2025-02-17 20:52:39 +0200

    transaction options style and render

--- src/Components/LatestTransitions.tsx
+++ src/Components/LatestTransitions.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "../styles/LatestTransitions.module.css";
 
 const LatestTransitions = () => {
+  const [activeTab, setActiveTab] = useState("Expenses");
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <p>Recent transactions</p>
         <button>+ Add transaction</button>
       </header>
+
+      <div className={styles.latestOptionsContainer}>
+        <div className={styles.options}>
+          {["Expenses", "Income", "Saving"].map((option) => (
+            <p
+              key={option}
+              className={activeTab === option ? `${styles.active}` : ""}
+              onClick={() => setActiveTab(option)}
+            >
+              {option}
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
