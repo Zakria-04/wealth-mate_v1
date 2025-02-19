8d62a7f6248cc27f1247743e97a59bcbb6370b55
Zakria-04 <zakriaabdelgny04@gmail.com>
2025-02-19 10:28:38 +0200

    handle new transaction api

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import transactionReducer from "./transactionSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    transaction: transactionReducer,
   },
 });
 
--- src/store/transactionSlice.ts
+++ src/store/transactionSlice.ts
@@ -1,7 +1,60 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createNewTransaction } from "@/res/api";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export const createTransactionFromAPI = createAsyncThunk(
+  "create/transaction",
+  async (body: any, { rejectWithValue }) => {
+    try {
+      const response = await createNewTransaction(body);
+      return response;
+    } catch (error: any) {
+      console.error("Transaction Error:", error);
+      return rejectWithValue(error.message || "Transaction failed");
+    }
+  }
+);
+
+interface TransactionState {
+  name: string;
+  category: string;
+  wallet: string;
+  sum: number;
+}
+
+interface InitialState {
+  transactions: TransactionState | {};
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: InitialState = {
+  transactions: {},
+  loading: false,
+  error: null,
+};
 
 const transactionSlice = createSlice({
   name: "transaction",
-  initialState: {},
+  initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createTransactionFromAPI.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        createTransactionFromAPI.fulfilled,
+        (state, action: PayloadAction<any>) => {
+          state.loading = false;
+          state.transactions = action.payload;
+        }
+      )
+      .addCase(createTransactionFromAPI.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      })
+  },
 });
+
+export default transactionSlice.reducer;
