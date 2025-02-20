c6b9f8ce453b4bf8da4886e5ffea171c2f60d8f1
Zakria-04 <zakriaabdelgny04@gmail.com>
2025-02-20 17:19:35 +0200

    date filter

--- src/Components/LatestTransitions.tsx
+++ src/Components/LatestTransitions.tsx
@@ -10,9 +10,7 @@ import {
 
 const LatestTransitions = () => {
   const [activeTab, setActiveTab] = useState("Expenses");
-  const { transactions, income, expenses, savings } = useSelector(
-    (state: RootState) => state.transaction
-  );
+  const { transactions } = useSelector((state: RootState) => state.transaction);
 
   if (!transactions) return null;
 
@@ -32,6 +30,20 @@ const LatestTransitions = () => {
         }
       });
   }, []);
+
+  // get categories
+  const getCategories = new Set(
+    transactions.map((transaction) => transaction.category)
+  );
+
+  // get transactions unique dates
+
+  const uniqueDates = new Set(
+    transactions.map((transaction) => {
+      const date = new Date(transaction.date);
+      return date.toISOString().split("T")[0];
+    })
+  );
 
   return (
     <div className={styles.container}>
@@ -55,8 +67,49 @@ const LatestTransitions = () => {
       </div>
 
       {/* category render and filter */}
-      <div>
-        <div></div>
+      <div className={styles.filterContainer}>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="11px"
+            viewBox="0 0 18 12"
+            width="17px"
+          >
+            <g fill="none" fillRule="evenodd">
+              <g fill="#3c3737">
+                <path d="M7,12 L11,12 L11,10 L7,10 L7,12 L7,12 Z M0,0 L0,2 L18,2 L18,0 L0,0 L0,0 Z M3,7 L15,7 L15,5 L3,5 L3,7 L3,7 Z" />
+              </g>
+            </g>
+          </svg>
+          <span>Filter by:</span>
+        </div>
+
+        <div className={styles.categoryContainer}>
+          {/* date filter */}
+          <div>
+            <select name="date" defaultValue="">
+              <option disabled value="">
+                Date
+              </option>
+              {Array.from(uniqueDates).map((getTransactionsDate) => {
+                console.log(getTransactionsDate);
+                
+                const transactionDate = new Date(getTransactionsDate);
+                const formattedDate =
+                  transactionDate.getDate() +
+                  "." +
+                  (transactionDate.getMonth() + 1) +
+                  "." +
+                  transactionDate.getFullYear();
+                return (
+                  <option key={getTransactionsDate} value={getTransactionsDate}>
+                    {formattedDate}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
       </div>
 
       {/* recent transition render */}
